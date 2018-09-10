428a6dfd78b3ecac899b4d5cd38c6fbe6d14ff1e
Ubuntu Desktop <agonzales43@mail.csuchico.edu.email>
2018-09-09 17:55:54 -0700

    part 1 and 2 complete

--- assin1/assignment1.py
+++ assin1/assignment1.py
@@ -1,8 +1,27 @@
+#!/usr/bin/env python
+from collections import defaultdict
+import collections
+import re
+
 
 #Part 1 of assignment
-def stringCount(aList):
-    return None
+#adapted from example found on https://www.quora.com/How-do-I-convert-a-list-into-a-dictionary-in-python
+#and https://www.saltycrane.com/blog/2007/09/how-to-sort-python-dictionary-by-keys/
+def stringCount(list):
+    set = collections.Counter(list)
+    for key in sorted(set.iterkeys()):
+        print "%s: %s" % (key, set[key])
 
 #Part 2 of assignment
 def isFloat(value):
-    return False
+    if re.match(r"^\d*[.]\d+$", value):
+        return True
+    else:
+        return False
+
+alist = ["Hello", "Hello", "Bob"]
+stringCount(alist)
+numFloat = isFloat("2.30")
+print(numFloat)
+numFloat = isFloat("2")
+print(numFloat)
